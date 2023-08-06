9c3f282089a0b5c27cb474ccef89fd5f541eabf9
kevinp83 <kevin.mail.pro@gmail.com>
2023-08-06 14:05:53 +0200

    Modif 2.0

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,11 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.static("../images"));
 
 // Type MIME acceptés et leurs extensions correspondantes
 const MIME_TYPES = {
@@ -6,11 +13,15 @@ const MIME_TYPES = {
     'image/jpeg': 'jpeg',
     'image/png': 'png'
 };
+
+
+
 // Configuration de multer pour la gestion des fichiers
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback(null, 'images') // Destination fichiers téléchargés dans dossier image
     },
+    
     filename: (req, file, callback) => {
         
         //Renommage du fichier pour éviter les espaces dans le nom
@@ -22,4 +33,24 @@ const storage = multer.diskStorage({
     }
 });
 
+const upload = multer({ storage });
+
+app.post("/", upload.single("picture"), async (req, res) => {
+    fs.access("./images", (error) => {
+      if (error) {
+        fs.mkdirSync("./images");
+      }
+    });
+    const { buffer, originalname } = req.file;
+    const timestamp = new Date().toISOString();
+    const ref = `${timestamp}-${originalname}.webp`;
+    await sharp(buffer)
+      .webp({ quality: 20 })
+      .toFile("./images/" + ref);
+      const link = `http://localhost:3000/${ref}`;
+        return res.json({ link });
+});
+
+app.listen(3000);
 module.exports = multer({ storage }).single('image');
+
